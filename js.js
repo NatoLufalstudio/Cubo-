ff53efed5f10aeb51e3cd4c3fc7b2fc43f012f34
NatoLufalstudio <38479245+NatoLufalstudio@users.noreply.github.com>
2018-07-12 11:37:15 -0500

    Update js.js

--- js.js
+++ js.js
@@ -284,9 +284,9 @@ Viewport.prototype.animate = function() {
 var viewport = new Viewport({
   element: document.getElementsByClassName('cube')[0],
   fps: 20,
-  sensivity: .1,
+  sensivity: 1.5,
   sensivityFade: .93,
-  speed: 0,
+  speed: 2,
   touchSensivity: 1.5
 });
 
